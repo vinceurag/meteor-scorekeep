b35c842ea3ceac92a5093e7a2a4413a3120b134a
Vince Urag <vinceuragvfx@gmail.com>
2017-09-03 22:47:27 +0800

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,5 @@
+// import './../imports/utils';
+import someDefault from './../imports/utils';
+
+console.log('Log from /client/main.js');
+console.log(someDefault);
---
+++ imports/utils.js
@@ -0,0 +1,9 @@
+console.log('Log from /imports/utils.js');
+
+export let greetUser = function() {
+  return 'Hello user!';
+};
+
+export let name = 'Vince Urag';
+
+export default 'Default val';
